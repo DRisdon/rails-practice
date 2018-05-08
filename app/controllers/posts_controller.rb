f09365073154b0a72b9036511327d56d1683693f
Daniel Risdon <drisdon07@gmail.com>
2018-05-08 12:46:17 -0400

    add create and destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,29 @@ class PostsController < ApplicationController
     render json: @post
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      render json: @post
+    else
+      puts @post.errors.messages.inspect
+      render status: :bad_request, json: {
+        errors: @post.errors.messages
+      }
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.delete
+    render json: {message: 'post deleted'}
+  end
+
+  private
+
+  def post_params
+    params.permit(:user, :content)
+  end
+
 end
